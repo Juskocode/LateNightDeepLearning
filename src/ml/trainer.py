a65c0597ba3480d34f130e1dc70a275128e29e2b
afreitas <adavidaf2002@gmail.com>
2025-06-14 03:39:54 +0100

    this is more stable

--- src/ml/trainer.py
+++ src/ml/trainer.py
@@ -6,39 +6,29 @@ import numpy as np
 
 
 class QTrainer:
-
     def __init__(self, model: nn.Module, learning_rate: float, gamma: float):
         self.model = model
+        self.target_model = type(model)(model.linear1.in_features,
+                                        model.linear1.out_features,
+                                        model.linear3.out_features)
+        self.target_model.load_state_dict(model.state_dict())
+
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.optimizer = optim.Adam(model.parameters(), lr=learning_rate)
         self.criterion = nn.MSELoss()
+        self.update_target_counter = 0
+        self.target_update_freq = 100
 
-    def train_step(
-            self,
-            state: Union[np.ndarray, List],
-            action: Union[np.ndarray, List],
-            reward: Union[float, List],
-            next_state: Union[np.ndarray, List],
-            done: Union[bool, List]
-    ) -> None:
-        """
-        Perform one training step using the Q-learning algorithm.
+    def train_step(self, state: Union[np.ndarray, List], action: Union[np.ndarray, List],
+                   reward: Union[float, List], next_state: Union[np.ndarray, List],
+                   done: Union[bool, List]) -> None:
 
-        Args:
-            state: Current state(s)
-            action: Action(s) taken
-            reward: Reward(s) received
-            next_state: Next state(s) after action
-            done: Whether the episode(s) ended
-        """
-        # Convert inputs to tensors
         state = torch.tensor(np.array(state), dtype=torch.float)
         next_state = torch.tensor(np.array(next_state), dtype=torch.float)
         action = torch.tensor(np.array(action), dtype=torch.long)
         reward = torch.tensor(np.array(reward), dtype=torch.float)
 
-        # Handle single sample case by adding batch dimension
         if len(state.shape) == 1:
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
@@ -46,20 +36,30 @@ class QTrainer:
             reward = torch.unsqueeze(reward, 0)
             done = (done,)
 
-        # Get predicted Q values for current states
+        # Double DQN implementation
         pred = self.model(state)
         target = pred.clone()
 
-        # Update Q values using Bellman equation
         for idx in range(len(done)):
             q_new = reward[idx]
             if not done[idx]:
-                q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                # Use main network to select action, target network to evaluate
+                next_q_values = self.model(next_state[idx])
+                best_action = torch.argmax(next_q_values)
+                target_q_values = self.target_model(next_state[idx])
+                q_new = reward[idx] + self.gamma * target_q_values[best_action]
 
             target[idx][torch.argmax(action[idx]).item()] = q_new
 
-        # Perform gradient descent
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
+
+        # Gradient clipping to prevent exploding gradients
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
         self.optimizer.step()
+
+        # Update target network periodically
+        self.update_target_counter += 1
+        if self.update_target_counter % self.target_update_freq == 0:
+            self.target_model.load_state_dict(self.model.state_dict())
