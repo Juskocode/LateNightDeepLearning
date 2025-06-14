ca4e4ebc9a60eefd9b53f9c43279390fd1f2e53f
afreitas <adavidaf2002@gmail.com>
2025-06-14 02:38:48 +0100

    constants s

---
+++ src/game/constants.py
@@ -0,0 +1,18 @@
+
+"""Game-specific constants and enums."""
+
+from enum import Enum
+from collections import namedtuple
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple('Point', 'x, y')
+
+# Game mechanics
+FRAME_TIMEOUT_MULTIPLIER = 100  # Max frames = 100 * snake_length
+FOOD_REWARD = 10
+COLLISION_PENALTY = -10
